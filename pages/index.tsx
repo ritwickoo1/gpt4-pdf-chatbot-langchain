9e63cb45103b035f9acfa15bf351a485f977e791
ritwickoo1 <ritwick.verma91@gmail.com>
2023-04-03 10:36:29 +0530

    update footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -319,9 +319,9 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
+          
+            Powered by ZentreeLabs.
+          
         </footer>
       </Layout>
     </>
